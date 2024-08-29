4b1a793acb6e88b9570340ae2b3bf33a216dffac
Roger Wang <>
2024-08-29 01:43:50 -0700

    Add banner page

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,20 +1,18 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Anchor, Box } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
-    <>
+    <Box className={classes.contentBox}>
       <Title className={classes.title} ta="center" mt={100}>
         Fight Health Insurance
       </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Vite project includes a minimal setup, if you want to learn more on Mantine +
-        Vite integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/vite/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit pages/Home.page.tsx file.
+      <Text c="dimmed" ta="center" size="lg" maw={720} mx="auto" mt="xl">
+        We'll help you to write an appeal to fight your health insurance denial. While an appeal is
+        not always the first step in the health insurance appeal process we'll guide you through the
+        options to fight back against health insurance denials. Almost all health plans are required
+        to offer internal and external appeals and while they often make it confusing we can help.
       </Text>
-    </>
+    </Box>
   );
 }
