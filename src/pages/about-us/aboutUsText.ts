389342a6f46d1044a908cc742c3988a75ec32b6d
Jenny Fang <jfang5792@gmail.com>
2024-10-28 16:57:48 -0700

    About Us text paragraphs will be stored in this file

---
+++ src/pages/about-us/aboutUsText.ts
@@ -0,0 +1,30 @@
+
+export const ABOUT_US_TEXT = [
+    `We are (exclusively) serving America, not because we don't love Europe, the rest of North America,
+    Africa, or any other place in the world, but because, frankly, American health insurance is a special kind of SNAFU.`,
+
+    `We at Totally Legit Co (of Delaware) decided to create "Fight Health Insurance" (sometimes called by… more colorful names)
+    because we're tired of dealing with health insurance denials. In our opinion, many health insurance denials are bogus,
+    and the insurance companies depend on us not appealing them. We hope that by making it easier to appeal medical health
+    insurance denials, we can change that calculus on insurance companies' part.`,
+
+    `Or, to put in another way, Fight Health Insurance is currently a passion project. We have day jobs, but most of us have
+    had terrible experiences with health insurance. We're tired of watching health insurance companies grind down our friends
+    (and tired of being ground down by them ourselves).`,
+
+    `This website is not intended to be legal advice. We are focused on providing information on how to appeal health insurance claims.
+    In some situations, such as Employee Retirement Income Security Act (ERISA), you may have the right to legal action, and you should consult with a lawyer.`,
+
+    `The results from Totally Legit Co come from a combination of custom-written templates, large language models, and (eventually) maybe more.
+    We strongly advise reading any appeal prior to submission for accuracy.`,
+
+    `Once we figure out the details and set up, we may make small amounts of money if you choose to use our fax or mailing services
+    (or we may lose money, honestly; we're figuring this out as we go). You can always choose to mail or fax letters yourself, but we believe
+    that by reducing the "friction" to filing an appeal, we can help create more appeals. We may also make money from links on this website
+    (through what is known as affiliate marketing).`,
+
+    `We also intend to offer a paid version (with an app) for doctors, therapists, and other practitioner offices, hospitals, etc. since they
+    deal with so many appeals. If that sounds like you (or your business), we would love your information to keep in touch (and even better,
+    a small amount of money if you're interested in being a first beta user).`
+
+  ];
