62ae23329ce22d7958aca923141dca899586dfce
Jenny Fang <jfang5792@gmail.com>
2024-11-20 20:55:24 -0800

    Create Homepage page. Removed excess step and added more description texts

---
+++ src/pages/homepage/Homepage.page.tsx
@@ -0,0 +1,46 @@
+import { Button, Center } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import { TextBlurb } from '../../components/TextBlurb/TextBlurb';
+import classes from '../styles.module.css';
+import { ScrollDownTag } from '../../components/ScrollDownTag/ScrollDownTag';
+import { UPLOAD_TEXT, WELCOME_TEXT, WELCOME_TITLE, APPEAL_TEXT, OVERTURN_TEXT } from './Homepage';
+
+export function HomePage() {
+  return (
+    <>
+      <div className={classes.snapSection}>
+        <TextBlurb title={WELCOME_TITLE} text={WELCOME_TEXT} />
+        <Center mt={80}>
+          <Button
+            radius="md"
+            size="xl"
+            className={classes.primaryColor}
+            component={Link}
+            to="/denial_questions"
+          >
+            Get Started
+          </Button>
+        </Center>
+        <ScrollDownTag label="How it works" />
+      </div>
+      <div className={classes.snapSection}>
+        <TextBlurb title="1. Upload your denial" text={UPLOAD_TEXT} additional={OVERTURN_TEXT} />
+        <ScrollDownTag label="Next step" />
+      </div>
+      <div className={classes.snapSection}>
+        <TextBlurb title="2. Get your appeal" text={APPEAL_TEXT} />
+        <Center mt={80}>
+          <Button
+            radius="md"
+            size="xl"
+            className={classes.primaryColor}
+            component={Link}
+            to="/denial_questions"
+          >
+            Get Started
+          </Button>
+        </Center>
+      </div>
+    </>
+  );
+}
