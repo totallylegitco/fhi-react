d983c2021e74701ae27462cb8b599fca7bc9d8f7
Jenny Fang <jfang5792@gmail.com>
2024-11-22 20:40:43 -0800

    Stack professional page text sections

--- src/components/TextBlurb/TextBlurb.tsx
+++ src/components/TextBlurb/TextBlurb.tsx
@@ -1,20 +1,32 @@
-import { Title, Text } from '@mantine/core';
+import { Title, Text, Stack } from '@mantine/core';
 import classes from './styles.module.css';
 
 interface TextBlurbProps {
   title: string;
-  text: string[];
+  text: string;
+  additional?: string[];
 }
 
-export function TextBlurb({ title, text }: TextBlurbProps) {
+export function TextBlurb({ title, text, additional }: TextBlurbProps) {
   return (
     <div>
       <Title className={classes.title} ta="center" mt={100}>
         {title}
       </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={720} mx="auto" mt="xl">
-        {text}
-      </Text>
+      {text && (
+        <Text c="dimmed" ta="center" size="lg" maw={720} mx="auto" mt="xl">
+          {text}
+        </Text>
+      )}
+      {additional && (
+        <Stack maw={820} mx="auto" mt="sm">
+          {additional.map((item, idx) => (
+            <Text key={idx} c="dimmed" ta="left" size="md">
+              {item}
+            </Text>
+          ))}
+        </Stack>
+      )}
     </div>
   );
 }
