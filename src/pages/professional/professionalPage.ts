b1e16ea617bcd4c79b41966c38943c4893435801
Jenny Fang <jfang5792@gmail.com>
2024-11-22 20:44:22 -0800

    Reformat professional text section into indented string sections to render as stack
    Indented due to prettier test check

---
+++ src/pages/professional/professionalPage.ts
@@ -0,0 +1,9 @@
+export const PRO_TITLE = 'Professional Version';
+
+export const PRO_TEXT = [
+  'We are actively working on a professional version.The available consumer version is not intended or certified for professional use. We would love to keep in touch via email to let you know when we have a professional version.',
+
+  'Not a professional (e.g. providers office, appeal specialist, lawyer, etc.) but still ended up here? Not to worry! You can try the regular appeal generator here.',
+
+  'If interested in being one of the first users, you can also give us $10 to get access for the first three months of the professional beta once we launch the professional version. If you change your mind at any time before or during the professional beta, we are more than happy to refund that. Honestly the $10 is to show that folks are willing to pay for it so we can get more resources.',
+];
